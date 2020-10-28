525d86566a61807ac79e9ccb7f3d3f50b4f0537b
Tom Meagher <tom@meagher.co>
2020-10-27 22:38:59 -0400

    feat: mobile styles

--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ const fetch = require('node-fetch')
 
 module.exports = async (req, res) => {
   const url = req.url === "/" || req.url === "/index.html" ? "/articles.html" : req.url
+  const head =
+    '<meta content="width=device-width, initial-scale=1.0" name="viewport" />' +
+    '<link media="all" href="/styles.css" rel="stylesheet" />' +
+    '<script src="/script.js"></script>' +
+    '</head>'
   const html = (
     await (await fetch('http://paulgraham.com' + url)).text()
-  ).replace(
-    '</head>',
-    '<link media="all" href="/styles.css" rel="stylesheet" /><script src="/script.js"></script></head>'
-  )
+  ).replace('</head>', head)
 
   res.send(html)
   res.end()
