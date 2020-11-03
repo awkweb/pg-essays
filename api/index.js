deb4af8e9e675edd36ded394322992b2fa37a478
Dario Vladovic <d.vladimyr@gmail.com>
2020-11-03 01:45:59 +0100

    Move html snippets outside of request handler

--- api/index.js
+++ api/index.js
@@ -3,6 +3,17 @@ const pkg = require('../package.json')
 
 const baseUrl = 'http://paulgraham.com'
 
+const head =
+    '<meta content="width=device-width, initial-scale=1.0" name="viewport" />' +
+    '<link media="all" href="/styles.css" rel="stylesheet" />' +
+    '<script src="/script.js"></script>' +
+    '</head>'
+
+const body =
+    `<footer><p>Site by <a href="https://meagher.co">Tom Meagher</a>. Source on <a href="${pkg.repository}">GitHub</a>.</p></footer>` +
+    '<script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>' +
+    '<noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" /></noscript>'
+
 module.exports = async (req, res) => {
     // Switch homepage to articles
     const isHomepage =
@@ -11,22 +22,9 @@ module.exports = async (req, res) => {
         req.url === '/articles.html'
     const url = isHomepage ? '/articles.html' : req.url
 
-    // Add new stylesheet and script
-    const head =
-        '<meta content="width=device-width, initial-scale=1.0" name="viewport" />' +
-        '<link media="all" href="/styles.css" rel="stylesheet" />' +
-        '<script src="/script.js"></script>' +
-        '</head>'
-
-    // Add footer and analytics
-    const body =
-        `<footer><p>Site by <a href="https://meagher.co">Tom Meagher</a>. Source on <a href="${pkg.repository}">GitHub</a>.</p></footer>` +
-        '<script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>' +
-        '<noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" /></noscript>'
-
     const html = (await (await fetch(new URL(url, baseUrl))).text())
-        .replace('</head>', head)
-        .replace('</body>', body)
+        .replace('</head>', head) // Add new stylesheet and script
+        .replace('</body>', body) // Add footer and analytics
         .replace(/�/g, '—') // Replace broken emdash
         .replace(/http:/g, "https:") // Force https for favicon, etc.
 
