da40dd94fd7b77da64627e11d2a0b689121d02dc
Dario Vladovic <d.vladimyr@gmail.com>
2020-11-03 02:38:05 +0100

    Fix client-side URL rewriting

--- public/script.js
+++ public/script.js
@@ -1,7 +1,22 @@
 setTimeout(() => {
+    // Normalize path
+    const path = normalize(location.pathname)
+    const basename = path.split('/').pop()
+    // Switch homepage to articles
+    const isHomepage = ['index.html', 'articles.html'].includes(basename)
+    const pathname = isHomepage ? 'articles.html' : path
     // Replace image with title with text
-    const imageEl = document.querySelector("tr td table tbody tr td img")
-    const titleEl = document.createElement("h1")
-    titleEl.innerHTML = `<a href="http://paulgraham.com${window.location.pathname}">${imageEl.alt}</a>`
+    const imageEl = document.querySelector('tr td table tbody tr td img')
+    const titleEl = document.createElement('h1')
+    const anchorEl = document.createElement('a')
+    anchorEl.href = new URL(pathname, 'http://paulgraham.com')
+    anchorEl.textContent = imageEl.alt
+    titleEl.appendChild(anchorEl)
     imageEl.parentNode.replaceChild(titleEl, imageEl)
+
+    function normalize(path) {
+        path = path.replace(/^\/|\/$/g, '')
+        path = path || 'index.html'
+        return path.replace(/(?<!(?:\.html?))$/, '.html')
+    }
 }, 0)
